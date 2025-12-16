e89197c7c3f7728850c40ed8c34dcb400a864f90
IsmaelTrentin <pandux66@gmail.com>
2025-12-16 21:16:14 +0100

    fix(lcd): fix lcd lib

--- src/libmc/lcd.c
+++ src/libmc/lcd.c
@@ -1,29 +1,30 @@
 #include <p32xxxx.h>
 #include "libmc/lcd.h"
+#include "libmc/timer.h"
 
 void lcd_init() {
     ANSELE = 0x0000; //RE0:7 as digital
-    TRISE = 0x00FF; // RE0:7 as digital input , or 0x0000 as out is the same
+    TRISE = 0x0000; // RE0:7 as digital input , or 0x0000 as out is the same
     TRISDbits.TRISD4 = 0; // RD4 as digital output ENpin
     TRISDbits.TRISD5 = 0; // RD5 as digital output RWpin
     ANSELBbits.ANSB15 = 0; //RB15 ad digital
-    TRISBbits.TRISB15 = 0;  // RB15 as digital output RSpin
-
+    TRISBbits.TRISB15 = 0; // RB15 as digital output RSpin
 
     // PMP initialization
     PMCON = 0x83BF; // Enable the PMP , long waits
     PMMODE = 0x3FF; // Master Mode 1
     PMAEN = 0x0001; // PMA0 enabled
 
+    timer1_delay_ms(20);
     PMADDR = LCDCMD; // command register ( ADDR = 0)
     PMDATA = 0x38; // 8 - bit interface , 2 lines , 5 x7
-    delay_ms_t1(1); // >48 us
+    timer1_delay_ms(1); // >48 us
     PMDATA = 0x0c; // ON , no cursor , no blink
-    delay_ms_t1(1); // >48 us
+    timer1_delay_ms(1); // >48 us
     PMDATA = 0x01; // clear display
-    delay_ms_t1(2); // >1.6 ms
+    timer1_delay_ms(2); // >1.6 ms
     PMDATA = 0x06; // increment cursor , no shift
-    delay_ms_t1(2); // >1.6 ms
+    timer1_delay_ms(2); // >1.6 ms
 }
 
 int lcd_pmp_is_busy() {
@@ -72,7 +73,6 @@ void lcd_line_2() {
 
 void lcd_print(char *s) {
     while (*s) {
-      
         lcd_printc(*s++);
     }
 }
