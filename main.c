1bca793af24a8561a66cbefe732790944c6b2620
IsmaelTrentin <pandux66@gmail.com>
2025-12-16 13:36:25 +0100

    init

---
+++ main.c
@@ -0,0 +1,21 @@
+/* Disable JTAG to use RA0 */
+#pragma config JTAGEN = OFF
+#pragma config FWDTEN = OFF
+/* Device Config Bits in DEVCFG1 : */
+#pragma config FNOSC = FRCPLL
+#pragma config FSOSCEN = OFF
+#pragma config POSCMOD = XT
+#pragma config OSCIOFNC = ON
+#pragma config FPBDIV = DIV_1
+/* Device Config Bits in DEVCFG2 : */
+#pragma config FPLLIDIV = DIV_2
+#pragma config FPLLMUL = MUL_20
+#pragma config FPLLODIV = DIV_2
+/* Enable clock config */
+#pragma config FCKSM = CSECMD
+
+#include <p32xxxx.h>
+
+void main() {
+    while (1);
+}
