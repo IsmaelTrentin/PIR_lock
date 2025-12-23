7592b5dd0d3aa03b0d43e60b1ca85137345a2f32
IsmaelTrentin <pandux66@gmail.com>
2025-12-23 22:25:49 +0100

    refactor: refac and fix uart lib, refac interrupts lib

--- include/libmc/uart.h
+++ include/libmc/uart.h
@@ -5,7 +5,7 @@ extern unsigned int uart_brg;
 void uart_config_pins();
 void uart_init(int baud, int priority, int subpriority);
 
-int uart_putc(int c);
+void uart_putc(int c);
 char uart_getc();
 void uart_puts(char data[]);
 void uart_gets(char dataOut[80]);
--- src/libmc/uart.c
+++ src/libmc/uart.c
@@ -8,8 +8,8 @@
 // UART_RX RF13
 
 unsigned int uart_brg = 0;
-int rx_got_byte = 0;
-char last_char = '\n';
+volatile int rx_got_byte = 0;
+volatile char last_char = '\n';
 
 void uart_config_pins() {
     // config TX
@@ -32,21 +32,32 @@ void uart_init(int baud, int priority, int subpriority) {
     U4BRG = uart_brg;
     U4STAbits.UTXEN = 1;
     U4STAbits.URXEN = 1;
+    // 8bit data no parity
+    U4MODEbits.PDSEL = 0b00; 
+    // 1 stop bit
+    U4MODEbits.STSEL = 0;
     U4MODEbits.ON = 1;
 
     // setup interrupt
     int_enable_uart(priority, subpriority);
 }
 
-void __attribute__((interrupt(ipl6), vector(INT_VN_UART4))) ihandler_uart4() {
-    if (int_getf_uart_rx() == 1) {
-        rx_got_byte = 1;
-        last_char = U4RXREG;
-        int_reset_uart_rx();
+void __attribute__((interrupt(ipl6), vector(_UART_4_VECTOR))) ihandler_uart4() {
+    if (IFS2bits.U4RXIF == 1) {
+        if (U4STAbits.OERR) {
+            U4STAbits.OERR = 0;
+        }
+
+        if (U4STAbits.URXDA) {
+            last_char = U4RXREG;
+            rx_got_byte = 1;
+        }
+
+        IFS2bits.U4RXIF = 0;
     }
 }
 
-int uart_putc(int c) {
+void uart_putc(int c) {
     while (U4STAbits.UTXBF == 1);
     U4TXREG = c;
 }
@@ -68,13 +79,18 @@ void uart_puts(char data[]) {
 
 void uart_gets(char data_out[LIBMC_UART_BUFF_SIZE]) {
     int i = 0;
+    char c;
 
-    char c = uart_getc();
-    while (c != '\r' && c != '\n' && i < LIBMC_UART_BUFF_SIZE) {
-        data_out[i] = c;
-        i++;
+    while (1) {
         c = uart_getc();
+
+        if (c == '\r' || c == '\n')
+            break;
+
+        if (i < LIBMC_UART_BUFF_SIZE - 1) {
+            data_out[i++] = c;
+        }
     }
-    i++;
+
     data_out[i] = '\0';
 }
